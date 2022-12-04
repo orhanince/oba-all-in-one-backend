83fd32e5427b7cb2314729c06952d57d180ff5a1
orhanince <ince.orhan@yandex.com>
2022-12-04 16:01:17 +0300

    Set up middleware

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,5 @@
 const healthy = require('./healthy.controller');
 const authController = require('./auth.controller');
-const todoController = require('./todo.controller');
-const formController = require('./form.controller');
 /**
  *
  * @param app {Application}
@@ -9,6 +7,4 @@ const formController = require('./form.controller');
 module.exports = (app) => {
   app.use('/', healthy);
   app.use('/auth', authController);
-  app.use('/todo', todoController);
-  app.use('/form', formController);
 };
--- src/models/index.js
+++ src/models/index.js
@@ -12,14 +12,7 @@
  * };
  */
 const { RequestToken } = require('./request_token');
-const { User } = require('./user');
-const { Todo } = require('./todo');
-const { Form } = require('./form');
-const { FormContent } = require('./form_content');
+
 module.exports = {
   RequestToken,
-  User,
-  Todo,
-  Form,
-  FormContent,
 };
