aff058bbf21c100c46436b97d207acac80456a6f
orhanince <ince.orhan@yandex.com>
2022-12-12 18:16:56 +0300

    Add publish form and delete form middleware methods

--- src/controllers/m.form.controller.js
+++ src/controllers/m.form.controller.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const mFormService = require('../services/m.form.service');
 const validatorMiddleware = require('../middlewares/validator-middleware');
-const { body } = require('express-validator');
+const { body, param } = require('express-validator');
 const auth = require('../middlewares/auth');
 
 /**
@@ -37,6 +37,63 @@ router.post(
   async (req, res, next) => {
     try {
       const result = await mFormService.createForm(req);
+      res.status(200).json(result);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+/**
+ * PUT /publish/{form_id}
+ */
+router.put(
+  '/publish/:form_id',
+  ...auth(),
+  validatorMiddleware(
+    param('form_id').isString().isLength({ min: 3, max: 100 })
+  ),
+  async (req, res, next) => {
+    try {
+      const result = await mFormService.publishForm(req);
+      res.status(200).json(result);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+/**
+ * PUT /
+ */
+router.put(
+  '/',
+  ...auth(),
+  validatorMiddleware(
+    body('form_name').isString().isLength({ min: 3, max: 100 }),
+    body('form_content').isString()
+  ),
+  async (req, res, next) => {
+    try {
+      const result = await mFormService.updateForm(req);
+      res.status(200).json(result);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+/** DELETE / */
+router.delete(
+  '/:form_id',
+  validatorMiddleware(
+    param('form_id').isString().isLength({ min: 3, max: 100 })
+  ),
+  ...auth(),
+  async (req, res, next) => {
+    try {
+      console.log('be', 1);
+      const result = await mFormService.deleteForm(req);
       res.status(200).json(result);
     } catch (e) {
       next(e);
--- src/services/m.form.service.js
+++ src/services/m.form.service.js
@@ -1,5 +1,16 @@
-const { ApiPOST, ApiGET, ServiceUrl } = require('./../utils/api-caller');
+const {
+  ApiPOST,
+  ApiGET,
+  ApiPUT,
+  ApiDELETE,
+  ServiceUrl,
+} = require('./../utils/api-caller');
 
+/**
+ * Get user's all form
+ * @param {Object} req
+ * @returns {Promise<*>}
+ */
 async function getAll(req) {
   let getUserAllFormResponse = await ApiGET(req, ServiceUrl.FORM, {
     route: '/form',
@@ -8,11 +19,16 @@ async function getAll(req) {
   return getUserAllFormResponse.data;
 }
 
+/**
+ * Create form
+ * @param {Object} req
+ * @returns {Promise<*>}
+ */
 async function createForm(req) {
   const { form_name, form_content } = req.body || {};
   let createFromResponse = await ApiPOST(req, ServiceUrl.FORM, {
     route: '/form',
-    withoutAuth: false,
+    withoutAuth: true,
     payload: {
       form_name: form_name,
       form_content: form_content,
@@ -21,7 +37,67 @@ async function createForm(req) {
   return createFromResponse.data;
 }
 
+/**
+ * Update form
+ * @param {Object} req
+ * @returns {Promise<*>}
+ */
+async function updateForm(req) {
+  const { form_name, form_content } = req.body || {};
+  let updateFormResponse = await ApiPUT(req, ServiceUrl.FORM, {
+    route: '/form',
+    withoutAuth: false,
+    payload: {
+      form_name: form_name,
+      form_content: form_content,
+    },
+  });
+  return updateFormResponse.data;
+}
+
+/**
+ * Publish form
+ * @param {Object} req
+ * @returns {Promise<*>}
+ */
+async function publishForm(req) {
+  const { form_id } = req.params || {};
+  let publishFromUpdate = await ApiPOST(req, ServiceUrl.FORM, {
+    route: '/form/publish',
+    withoutAuth: true,
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: req.headers.authorization,
+    },
+    payload: {
+      form_id: form_id,
+    },
+  });
+
+  return publishFromUpdate.data;
+}
+
+/**
+ * Delete form
+ * @param {Object} req
+ * @returns {Promise<*>}
+ */
+async function deleteForm(req) {
+  const { form_id } = req.params || {};
+  let deleteFormResponse = await ApiDELETE(req, ServiceUrl.FORM, {
+    route: '/form',
+    withoutAuth: true,
+    payload: {
+      form_id: form_id,
+    },
+  });
+  return deleteFormResponse.data;
+}
+
 module.exports = {
   createForm,
   getAll,
+  updateForm,
+  publishForm,
+  deleteForm,
 };
